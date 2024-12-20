6c2a1c096e39360934d19615d0dffcc913b7b43a
ElBewBew <benoit.vanhoe@gmail.com>
2024-12-20 11:07:42 +0100

    ajout de l'ombre sur les blocs, ajout du bouton supp par encore fonctionnel

--- gdb_mobile/lib/view/Recap.dart
+++ gdb_mobile/lib/view/Recap.dart
@@ -45,9 +45,10 @@ class _RecapPageState extends State<RecapPage> {
             ),
             boxShadow: [
               BoxShadow(
-                color: Color.fromARGB(133, 226, 226, 226),
-                offset: Offset(0, 3),
-                blurRadius: 5,
+                color: Color.fromARGB(244, 53, 121, 248),
+                offset: Offset(0, -50),
+                blurRadius: 80,
+                spreadRadius: -70,
               ),
             ],
           ),
@@ -80,62 +81,94 @@ class _RecapPageState extends State<RecapPage> {
                       return Column(
                         children: documents.map((doc) {
                           final data = doc.data() as Map<String, dynamic>;
-                          return Container(
-                            margin: const EdgeInsets.symmetric(vertical: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(
-                                color: Colors.blueAccent,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(15),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.1),
-                                  blurRadius: 8,
-                                  spreadRadius: 2,
-                                  offset: const Offset(0, 4),
+                          return Stack(
+                            clipBehavior: Clip.hardEdge, // Ajoutez cette ligne pour masquer le débordement
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.symmetric(vertical: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                    color: Colors.blueAccent,
+                                    width: 2,
+                                  ),
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                              ],
-                            ),
-                            padding: const EdgeInsets.all(12),
-                            child: Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/${data['type'].toLowerCase()}.png',
-                                  width: 40,
-                                  height: 40,
-                                ),
-                                const SizedBox(width: 16),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                padding: const EdgeInsets.all(12),
+                                child: Row(
                                   children: [
-                                    Text(
-                                      'Type: ${data['type']}',
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.blueAccent,
-                                      ),
+                                    Image.asset(
+                                      'assets/images/${data['type'].toLowerCase()}.png',
+                                      width: 40,
+                                      height: 40,
                                     ),
-                                    Text(
-                                      'Montant: ${data['montant']} €',
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.black,
-                                      ),
+                                    const SizedBox(width: 16),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Type: ${data['type']}',
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600,
+                                            color: Colors.blueAccent,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Montant: ${data['montant']} €',
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Date: ${data['dateOperation']}',
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      'Date: ${data['dateOperation']}',
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.black,
+                                    const Spacer(),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                        colors: [Colors.black.withOpacity(0.2), const Color.fromARGB(172, 126, 126, 126)],
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight,
+                                      ),
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: IconButton(
+                                        icon: const Icon(Icons.close, color: Colors.white),
+                                        onPressed: () {
+                                          _noteFraisCollection.doc(doc.id).delete();
+                                        },
                                       ),
                                     ),
                                   ],
                                 ),
-                              ],
-                            ),
+                              ),
+                              Positioned(
+                                top: 0,
+                                bottom: 0,
+                                right: 0,
+                                left: 0,
+                                child: ClipPath(
+                                  clipper: DiagonalClipper(),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: [Colors.black.withOpacity(0.2), const Color.fromARGB(0, 10, 151, 207)],
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           );
                         }).toList(),
                       );
@@ -148,5 +181,23 @@ class _RecapPageState extends State<RecapPage> {
         ),
       ),
     );
+  }
+}
+
+class DiagonalClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, size.height * 2);
+    path.lineTo(size.width, size.height * -0.1);
+    path.lineTo(size.width, size.height - 0.1);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
